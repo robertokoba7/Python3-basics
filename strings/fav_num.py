f02a6b5b556b9023e1388e5cf70105c1b70eb1d4
robertokoba7 <okobarobert7@gmail.com>
2023-12-08 23:48:48 +0300

    using str()method to convert int to str b4 cat

---
+++ strings/fav_num.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Qn. 2-9. Favorite Number: Store your favorite number in a variable. Then, using
+that variable, create a message that reveals your favorite number. Print that
+message.
+- N/B: Tests my knowlege of str() function
+"""
+
+fav_num = 7
+message = 'My favourite number is ' + str(fav_num) + 'th number in a series of integers.'
+
+print(message)
