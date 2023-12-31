bc48e5368b3bf58c97d715052c97ee083b23d17c
robertokoba7 <okobarobert7@gmail.com>
2023-12-31 10:19:36 +0300

    A tale of two sequences

---
+++ Tuples/two_sequences.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""This is a tale of two sequences"""
+l = list()
+dir(l)
+['append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+t = tuple()
+dir(t)
+['count', 'index']
