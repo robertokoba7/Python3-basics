b03e8a30e803095a5fc27502b737067cd859f0ab
robertokoba7 <okobarobert7@gmail.com>
2024-01-14 20:39:02 +0300

    loop thru al k-v pairs

---
+++ Dictionaries/user_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""looping through all key-value pairs"""
+user_0 = {
+
+        'username' : 'riciibob',
+        'firstname' : 'bob',
+        'lastname' : 'ricii',
+}
+for key,value in user_0.items():
+    print('\nKey: ' + key)
+    print('Value: ' + value)
+
