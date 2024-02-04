821b3d6830d5d3b74e58ac7c8b3be031f7e4dd5e
robertokoba7 <okobarobert7@gmail.com>
2024-02-04 22:12:06 +0300

    using in operator in a dictionary

---
+++ Dictionaries/states_population.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This challenge asks you to build a dictionary and use keys into that dictionary to extract
+information. The information is given as a table of state names (keys) and the population
+of each state (values). The program should allow the user to enter the name of a state.
+If the state is found in the dictionary, then the program should report the population of
+that state. If the state is not found, then the program should output a message like “Sorry,
+but we do not have information for that state.” The program should run in a loop, allow
+the user to enter any number of states, and then exit when the user presses Return (Mac)
+or Enter (Windows). As of 2018, the data for the 12 states with the highest populations is
+shown in Table 11-1.
+"""
+popultnDict = {'California': 39776830, 'Texas': 28704330, 'Florida': 21312211, 'New York': 19862512,\
+        'Pennsylvania': 12823989, 'Illinois': 12768320, 'Ohio': 11694664, 'Georgia': 10545138,\
+        'North Carolina': 10390149, 'Michigan': 9991177, 'New Jersey': 9032872, 'Virginia': 8525660}
+
+
+user_input = input('Enter the name of the state: ')
+state = popultnDict.keys()
+if user_input in state:
+    print(popultnDict[user_input])
+else:
+    print('Sorry, but we do not have information for that state.')
+
